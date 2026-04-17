f90f256be848b4f351ad31b0f9e4edbe977cdca9
blancayessenia <you@example.com>
2026-04-16 17:34:00 -0700

    actualizacion

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 
-from .models import Comment
+from.models import Comment
 
-class ComentFrom(forms.ModelForm):
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name','email','body']
 
-    class meta:
-        model = Commentfields = ['name','email','body']
-        
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-#-*- coding: utf-8-*-
-
+# -*- coding: utf-8 -*-
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Post, Category
@@ -7,17 +7,17 @@ from .forms import CommentForm
 
 # Create your views here.
 def home(request):
-
     posts = Post.objects.filter(status=Post.ACTIVE).order_by('-created_at')
     context = {
         'posts': posts
     }
     return render(request, 'blog/home.html', context)
+
 def detail(request, id):
-    post = get_object_or_404(Post, id = id, status=Post.ACTIVE)
-    
-    if request.method =='POST':
-        form = CommentForm (request.POST)
+    post = get_object_or_404(Post, id=id, status=Post.ACTIVE)
+
+    if request.method == 'POST':    
+        form = CommentForm(request.POST)
 
         if form.is_valid():
             comment = form.save(commit=False)
@@ -25,9 +25,13 @@ def detail(request, id):
             comment.save()
 
             return redirect('post_detail')
+    else:
+        form=CommentForm()    
 
     context = {
         'post': post,
-    }
-    return render(request, 'blog/detail.html', context)
+        'form': form
+    }  
+    return render (request, 'blog/detail.html',context)
+
 
